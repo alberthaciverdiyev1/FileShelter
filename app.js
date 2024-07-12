4bb3db6d518db8c5d89f4c57fba94216579f3c9f
alberthaciverdiyev1 <7cw5lnl@code.edu.az>
2024-07-12 10:48:38 -0700

    logger js env

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const router = require('./app/routes/main');
 const cookieParser = require('cookie-parser');
 const express = require('express');
 const app = express();
-const loggerMiddleware = require('./app/middlewares/logger.js');
 
 require('./app/configs/database.js');
 require('./app/middlewares/globalErrorHandling.js');
@@ -14,13 +13,14 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use('/', router);
 
-if(process.env.NODE_ENV !== 'production'){
+if (process.env.NODE_ENV !== 'production') {
+  const loggerMiddleware = require('./app/middlewares/logger.js');
   app.use(loggerMiddleware);
 }
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-app.listen(8888, () => {
-  console.log(`FileShelter API ${8888} !`);
+app.listen(3000, () => {
+  console.log(`FileShelter API ${3000} !`);
 });
--- app/helpers/logger.js
+++ app/helpers/logger.js
@@ -1,17 +1,19 @@
-const winston = require('winston');
-require('winston-daily-rotate-file');
-const { combine, timestamp, json } = winston.format;
+if (process.env.NODE_ENV !== 'production') {
+    const winston = require('winston');
+    require('winston-daily-rotate-file');
+    const { combine, timestamp, json } = winston.format;
 
-const fileRotateTransport = new winston.transports.DailyRotateFile({
-    filename: 'app/logs/combined-%DATE%.log',
-    datePattern: 'YYYY-MM-DD',
-    maxFiles: '14d',
-});
+    const fileRotateTransport = new winston.transports.DailyRotateFile({
+        filename: 'app/logs/combined-%DATE%.log',
+        datePattern: 'YYYY-MM-DD',
+        maxFiles: '14d',
+    });
 
-const logger = winston.createLogger({
-    level: process.env.LOG_LEVEL || 'info',
-    format: combine(timestamp(), json()),
-    transports: [fileRotateTransport],
-});
+    const logger = winston.createLogger({
+        level: process.env.LOG_LEVEL || 'info',
+        format: combine(timestamp(), json()),
+        transports: [fileRotateTransport],
+    });
 
-module.exports = logger;
+    module.exports = logger;
+}
