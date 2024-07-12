28a5b9c65a9c45110b8414e4d395574f52f236d6
alberthaciverdiyev1 <7cw5lnl@code.edu.az>
2024-07-12 10:45:50 -0700

    gloprocess.env.NODE_ENV !== 'production'

--- app/middlewares/globalErrorHandling.js
+++ app/middlewares/globalErrorHandling.js
@@ -1,17 +1,19 @@
-const logger = require('../helpers/logger.js');
-
-process.on('uncaughtException', (err) => {
-  logger.error({
-    message: err.message,
-    stack: err.stack,
+if (process.env.NODE_ENV !== 'production') {
+    const logger = require('../helpers/logger.js');
+  
+  process.on('uncaughtException', (err) => {
+    logger.error({
+      message: err.message,
+      stack: err.stack,
+    });
+    process.exit(1);
   });
-  process.exit(1);
-});
-
-process.on('unhandledRejection', (reason, promise) => {
-  logger.error({
-    message: 'Unhandled Rejection',
-    reason: reason,
-    promise: promise,
+  
+  process.on('unhandledRejection', (reason, promise) => {
+    logger.error({
+      message: 'Unhandled Rejection',
+      reason: reason,
+      promise: promise,
+    });
   });
-});
+}
