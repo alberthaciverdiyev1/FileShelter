93d141334e2083cf21b6fdb53044dbcc5dc79a3e
alberthaciverdiyev1 <7cw5lnl@code.edu.az>
2024-07-19 14:08:40 -0700

    message view css and ejs

--- assets/js/pages/chat/socket.js
+++ assets/js/pages/chat/socket.js
@@ -32,5 +32,21 @@ document.addEventListener('DOMContentLoaded', function (e) {
     });
 
 
+    const toggleButton = document.querySelector('.dark-light');
+const colors = document.querySelectorAll('.color');
+
+colors.forEach(color => {
+  color.addEventListener('click', (e) => {
+    colors.forEach(c => c.classList.remove('selected'));
+    const theme = color.getAttribute('data-color');
+    document.body.setAttribute('data-theme', theme);
+    color.classList.add('selected');
+  });
+});
+
+toggleButton.addEventListener('click', () => {
+  document.body.classList.toggle('dark-mode');
+});
+
 });
 
