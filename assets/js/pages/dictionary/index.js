f136129c6614b83158eb44afc179dcf64a1deda1
alberthaciverdiyev1 <alberthaciverdiyev55@gmail.com>
2024-08-31 08:46:03 -0700

    changed js dictionary

--- assets/js/pages/dictionary/index.js
+++ assets/js/pages/dictionary/index.js
@@ -1,43 +1,57 @@
 document.addEventListener("DOMContentLoaded", function (e) {
-    const tableHtml = (data) => {        
+    const tableHtml = (data) => {
         let h = '';
         data.map((v, i) => {
-            h += ` <tr class="bg-gray-100 border-b text-black">
-                    <th class="px-1 py-2 text-sm sm:text-base text-center w-8 border border-orange-200">
-                        ${i + 1}
-                    </th>
-                    <td class="px-3 py-2 text-sm sm:text-base border border-orange-200">
-                        ${v.foreignWord}
-                    </td>
-                    <td class="px-3 py-2 text-sm sm:text-base border border-orange-200">
-                        ${v.translatedWord}
-                    </td>
-                    <td class="px-3 py-2 text-sm sm:text-base border border-orange-200">
-                        ${v.description}
-                    </td>
-                    <td class="px-3 py-2 text-sm sm:text-base border border-orange-200 sm: w-8">
-                    <div class="flex flex-row flex-wrap space-x-2">
-                        <button class="py-1 px-3 text-xs sm:text-xs text-white bg-yellow-500 border border-gray-300 rounded-lg hover:bg-yellow-700 transition duration-300 ease-in-out flex items-center justify-center">
-                            <i class="fas fa-edit mr-1"></i>
-                            <span class="hidden sm:inline">Edit</span>
-                        </button>
-                    </div>
-                </td>
-                    <td class="px-3 py-2 text-sm sm:text-base border border-orange-200 sm: w-8">
-                    <div class="flex flex-row flex-wrap space-x-2">
-                        <button class="py-1 px-3 text-xs sm:text-xs text-white bg-red-500 border border-gray-300 rounded-lg hover:bg-red-700 transition duration-300 ease-in-out flex items-center justify-center">
-                            <i class="fas fa-trash mr-1"></i>
-                            <span class="hidden sm:inline">Delete</span>
-                        </button>
-                    </div>
-                </td>
+            // h += ` <tr class="bg-gray-100 border-b text-black">
+            //         <th class="px-1 py-2 text-xs sm:text-base text-center w-8 border border-orange-200">
+            //             ${i + 1}
+            //         </th>
+            //         <td class="px-3 py-2 text-xs sm:text-base border border-orange-200">
+            //             ${v.foreignWord}
+            //         </td>
+            //         <td class="px-3 py-2 text-xs sm:text-base border border-orange-200">
+            //             ${v.translatedWord}
+            //         </td>
+            //         <td class="px-3 py-2 text-xs sm:text-base border border-orange-200">
+            //             ${v.description}
+            //         </td>
+            //         <td class="px-3 py-2 text-xs sm:text-base border border-orange-200 sm: w-8">
+            //         <div class="flex flex-row flex-wrap space-x-2">
+            //             <button class="py-1 px-3 text-xs sm:text-xs text-white bg-yellow-500 border border-gray-300 rounded-lg hover:bg-yellow-700 transition duration-300 ease-in-out flex items-center justify-center">
+            //                 <i class="fas fa-edit mr-1"></i>
+            //                 <span class="hidden sm:inline">Edit</span>
+            //             </button>
+            //         </div>
+            //     </td>
+            //         <td class="px-3 py-2 text-xs sm:text-base border border-orange-200 sm: w-8">
+            //         <div class="flex flex-row flex-wrap space-x-2">
+            //             <button class="py-1 px-3 text-xs sm:text-xs text-white bg-red-500 border border-gray-300 rounded-lg hover:bg-red-700 transition duration-300 ease-in-out flex items-center justify-center">
+            //                 <i class="fas fa-trash mr-1"></i>
+            //                 <span class="hidden sm:inline">Delete</span>
+            //             </button>
+            //         </div>
+            //     </td>
+            //     </tr> `;
 
-
-                </tr> `;
+            h += `
+     <tr class="text-start min-w-[340px]  bg-gray-50" ${v._id}>
+        <td class="border-l border-r border-b border-orange-200 px-2 py-3 w-9">${i + 1}</td>
+        <td class="border-l border-r border-b border-orange-200 px-2 py-3">${v.foreignWord ? v.foreignWord.slice(0, Math.min(13, v.foreignWord.length)) : ''}</td>
+        <td class="border-l border-r border-b border-orange-200 px-2 py-3">${v.translatedWord ? v.translatedWord.slice(0, Math.min(13, v.translatedWord.length)) : ''}</td>
+        <td class="border-l border-r border-b border-orange-200 px-2 py-3">${v.description ? v.description.slice(0, Math.min(13, v.description.length)) : ''}</td>
+        <td class="border-l border-r border-b border-orange-200 px-2 py-3 w-10">
+            <div class="flex flex-row flex-wrap space-x-2 justify-center">
+                <button class="py-1 px-3 text-xs sm:text-xs text-white bg-red-500 border border-gray-300 rounded-lg hover:bg-red-700 transition duration-300 ease-in-out flex items-center">
+                    <i class="fas fa-trash mr-1"></i>
+                    <span class="hidden sm:inline">Delete</span>
+                </button>
+            </div>
+        </td>
+    </tr>`;
         });
-        
 
-        document.getElementById('topics-table-body').innerHTML = h;
+
+        document.getElementById('dictionary-body').innerHTML = h;
     }
 
 
